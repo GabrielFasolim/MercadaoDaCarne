98e5cdaa77aa2f824cf24e3889a2ff3ba8a39c61
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-06-17 23:07:21 -0300

    [add]: start header

---
+++ src/pages/home/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Css Links -->
+  <link rel="stylesheet" href="../../css/style.css" />
+  <link rel="stylesheet" href="../../css/reset.css" />
+
+  <!-- MDB -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css"
+    rel="stylesheet"
+  />
+
+  <title>Mercadão da carne</title>
+</head>
+
+<body>
+  <header class="header__container">
+    <div class="header-infos"></div>
+    <div class="header-content container-md">
+      <img class="img-fluid" src="../../img/logo.png" alt="Logo mercadão da carne">
+      <div class="content-principal">
+        <div class="principal-search">
+          <div class="categories-nav"><p>categorias</p><p>Veja nossos Produtos</p></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <div class="header-extra"></div>
+  </header>
+
+  <!-- MDB -->
+  <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"
+  ></script>
+
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</body>
+</html>
