e1f02cab4977a814d213d1b5871e7e32c2804c16
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-06-22 21:15:05 -0300

    [update]: add header on component

--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -31,40 +31,8 @@
 </head>
 
 <body>
-  <header class="header__container">
-    <div class="header-infos">
-      <i class="fas fa-location-dot"></i>
-      <p>Cidade - Estado</p>
-    </div>
-    <div class="header-content container-md">
-      <img class="img-fluid" src="../../img/logo.png" alt="Logo mercadão da carne">
-      <div class="content-principal">
-        <div class="principal-search">
-          <div class="categories-nav"><p>categorias</p><p>Veja nossos Produtos</p></div>
-          <div class="categories-search"><input type="text" placeholder="Procurar..."><i class="bi bi-search"></i></div>
-        </div>
-        <div class="principal-nav">
-          <ul class="principal-categories">
-            <li><i class="icon-menu icon-bovinos"></i><p>bovinos</p></li>
-            <li><i class="icon-menu icon-aves"></i><p>aves</p></li>
-            <li><i class="icon-menu icon-suinos"></i><p>suínos</p></li>
-            <li><i class="icon-menu icon-linguicas"></i><p>linguiças</p></li>
-            <li><i class="icon-menu icon-kits"></i><p>kits</p></li>
-            <li><i class="icon-menu icon-tudo"></i><p>tudo</p></li>
-          </ul>
-        </div>
-      </div>
-      <div class="responsive-menu">
-        <i class="fas fa-magnifying-glass fa-lg me-2"></i>
-        <i class="fas fa-bars fa-lg"></i>
-      </div>
-    </div>
-    <div class="header-extra">
-      <i class="fas fa-truck"></i>
-      <p>Frete agendado e entrega em 1 ou 2 dias úteis.</p>
-    </div>
-  </header>
-
+  <?php require_once '../../components/header.php'; ?>
+  
   <!-- MDB -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
