e57f9800267f2e1d6717452e83e6a8bc66d0f2da
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-07-11 20:35:15 -0300

    [add]: table responsive

---
+++ src/pages/admin/table/listProducts.php
@@ -0,0 +1,42 @@
+<?php
+  require_once "../../../database/connection.php";
+
+  $sql = "SELECT c.id, c.nome, c.descricao, c.preco, t.nome AS tipo_nome 
+          FROM mercadaodacarne.carnes c 
+          LEFT JOIN mercadaodacarne.tipo t ON c.tipo_id = t.id";
+
+  if ($result = mysqli_query($conn, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)){
+            echo "<tr class='panelContent'>";
+            echo "<th class='id' scope='row'>$row[id]</th>";
+            echo "<td>$row[nome]</td>";
+            echo "<td class='descricao'>$row[descricao]</td>";
+            echo "<td>$row[tipo_nome]</td>";
+            echo "<td>$row[preco]</td>";
+            echo "<td>
+                  <a>
+                    <i class='fas fa-trash'></i>
+                  </a>
+                  <a>
+                    <i class='fas fa-pen'></i>
+                  </a>
+                </td>";
+            echo "</tr>";
+        }
+    }
+} 
+
+?>
+
+<script>
+    var descricaoElements = document.getElementsByClassName("descricao");
+    for (var i = 0; i < descricaoElements.length; i++) {
+        var descricao = descricaoElements[i].textContent;
+        var limiteCaracteres = 100;
+
+        if (descricao.length > limiteCaracteres) {
+            descricaoElements[i].textContent = descricao.substring(0, limiteCaracteres) + "...";
+        }
+    }
+</script>
--- src/pages/admin/table/productTable.php
+++ src/pages/admin/table/productTable.php
@@ -1,58 +1,68 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Css Links -->
-  <link rel="stylesheet" href="../../../css/style.css" />
-  <link rel="stylesheet" href="../../../css/reset.css" />
+    <!-- Css Links -->
+    <link rel="stylesheet" href="../../../css/style.css" />
+    <link rel="stylesheet" href="../../../css/reset.css" />
 
-  <!-- Font Awesome -->
-  <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-  />
-  
-  <!-- Google Fonts -->
-  <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-  />
+    <!-- Font Awesome -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      rel="stylesheet"
+    />
+    
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      rel="stylesheet"
+    />
 
-  <!-- MDB -->
-  <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css"
-    rel="stylesheet"
-  />
+    <!-- MDB -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css"
+      rel="stylesheet"
+    />
 
-  <title>Mercadão da carne</title>
-</head>
-<body>
-  <main class="container-table">
-    <div class="tableContent">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Categoria</th>
-            <th scope="col">Preço</th>
-            <th scope="col">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-      
-        </tbody>
-      </table>
-    </div>
-  </main>
-  
-  <!-- MDB -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  
-</body>
+    <title>Mercadão da carne</title>
+  </head>
+  <body>
+    <?php
+      require_once "../../../components/adminAuthorization.php"
+    ?>
+    <main class="container-table">
+      <div class="tableContent">
+        <div class="btns mb-3">
+          <a href="" class="btn btn-outline-danger"><i class="fas fa-eye"></i> Ver categorias</a>
+          <a href="" class="btn btn-danger"><i class="fas fa-square-plus"></i> Adicionar item</a>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-striped text-center table-panel">
+            <thead class="table-light">
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Categoria</th>
+                <th scope="col">Preço</th>
+                <th scope="col">Ações</th>
+              </tr>
+              <?php include "./listProducts.php" ?>
+            </thead>
+            <tbody>
+          
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
+    
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    
+  </body>
 </html>
