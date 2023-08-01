1e094921140fdfd618a44411f72ca5b5bc0d144c
Gabriel Fasolim <gabrielfasolim@hotmail.com>
2023-08-01 10:38:53 -0300

    update click

--- src/pages/admin/table/updateCategoryExe.php
+++ src/pages/admin/table/updateCategoryExe.php
@@ -1,44 +1,49 @@
 <?php
-session_start();
-require_once "../../../database/connection.php";
+    session_start();
+    require_once "../../../database/connection.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = $_POST['id'];
-    $tipoNome = $_POST['nome'];
-    $descricao = $_POST['descricao'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $id = $_POST['id'];
+        $tipoNome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
 
-    $id = mysqli_real_escape_string($conn, $id);
-    $tipoNome = mysqli_real_escape_string($conn, $tipoNome);
-    $descricao = mysqli_real_escape_string($conn, $descricao);
+        $id = mysqli_real_escape_string($conn, $id);
+        $tipoNome = mysqli_real_escape_string($conn, $tipoNome);
+        $descricao = mysqli_real_escape_string($conn, $descricao);
 
-    $sql = "UPDATE tipo
-            SET nome = '$tipoNome',
-                descricao = '$descricao'
-            WHERE id = '$id'";
+        $sql = "UPDATE tipo
+                SET nome = '$tipoNome',
+                    descricao = '$descricao'
+                WHERE id = '$id'";
 
-    if (mysqli_query($conn, $sql)) {
-        $status = 'success';
-        $message = 'Categoria atualizada com sucesso';
-        $statusCode = 200;
-    } else {
-        $status = 'error';
-        $message = 'Erro ao atualizar a categoria';
-        $statusCode = 500;
-    }
+        if ($result = mysqli_query($conn, $sql)) {       
+            $status = 'success';
+            $message = 'Requisição bem sucedida';
+            $statusCode = 200;
 
-    $response = array(
-        'status' => $status,
-        'message' => $message,
-    );
+            $response = array(
+                'status' => $status,
+                'message' => $message,
+            );
+            echo json_encode($response);
+            http_response_code($statusCode);
+            header("location: categoryTable.php");
+            exit;
+        } else {
+            $status = 'error';
+            $message = 'Erro ao realizar a requisição';
+            $statusCode = 500;
 
-    http_response_code($statusCode);
-    echo json_encode($response);
+            $response = array(
+                'status' => $status,
+                'message' => $message,
+            );
+            echo json_encode($response);
+            http_response_code($statusCode);
+            exit;
+            }
+        }
 
-    exit;
-} else {
-    header("Location: categoryTable.php");
-    exit; 
-}
 
-mysqli_close($conn);
+    mysqli_close($conn);
 ?>
