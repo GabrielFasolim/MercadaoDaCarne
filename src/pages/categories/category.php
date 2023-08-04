9bd58db6472740ca20fc132cc71fc037ba4a2654
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-03 21:47:40 -0300

    [add]: categoryExe

---
+++ src/pages/categories/category.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Css Links -->
+  <link rel="stylesheet" href="../../css/reset.css" />
+  <link rel="stylesheet" href="../../css/style.css" />
+
+  <!-- Font Awesome -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+  />
+  
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+  />
+
+  <!-- MDB -->
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.css"
+    rel="stylesheet"
+  />
+
+  <title>Mercadão da carne</title>
+</head>
+
+<body>
+  <?php require_once '../../components/header.php'; ?>
+  <main class="container-main">
+  </main>
+  
+  <!-- MDB -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  
+</body>
+</html>
---
+++ src/pages/categories/categoryExe.php
@@ -0,0 +1,35 @@
+<?php
+
+  require_once '../../database/connection.php';
+
+  $category = $_GET[category];
+
+  $sql = "SELECT c.id, c.nome, c.preco, c.foto, t.nome AS tipo_nome
+          FROM carnes c
+          INNER JOIN tipo t ON c.tipo_id = t.id
+          WHERE t.nome LIKE '%$category%'";
+
+  if($result = mysqli_query($conn, $sql)) {
+    if(mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_assoc($result)) {
+        echo '<div class="product">';
+          echo "<img src='data:image/jpeg;base64, " . $row['foto'] . "' alt='' class='img-fluid profile-pic'>";
+          echo '<div class="category-and-name">';
+            echo "<p class='product-category'>$row[tipo_nome]</p>";
+            echo "<p class='product-name'>$row[nome]</p>";
+          echo '</div>';
+          echo '<div class="price-and-kg">';
+            echo "<p class='product-price'>R$ $row[preco]</p>";
+            echo "<p class='product-kg'>KG</p>";
+          echo '</div>';
+          echo '<hr>';
+          echo "<a href='../../pages/products/products.php?id=$row[id]' class='btn'><i class='fas fa-basket-shopping'></i> Comprar</a>";
+        echo '</div>';
+      }
+    } else {
+      echo "<p class='null'>Não foi encontrado nenhum produto cadastrado!</p>";
+    }
+  }
+
+
+?>
