91f5203df7db99ffe8e31986e5aede6e1527a48f
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-06 00:52:41 -0300

    [add]: footer completed

---
+++ src/components/footer.php
@@ -0,0 +1,58 @@
+<footer class="container__foter" class="text-center text-lg-start text-dark">
+  <section class="footer__section">
+      <div class="d-flex align-items-center justify-content-center p-3 text-white gap-2">
+        <a target="_blank" class="link" href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a target="_blank" class="link" href="#">
+          <i class="fab fa-whatsapp"></i>
+        </a>
+      </div>
+  </section>
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold">mercadão barato</h6>
+          <hr class="mb-3 mt-0 d-inline-block mx-auto"/>
+          <p class="mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold"><i class="bi bi-house-door-fill"></i> home</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto"/>
+          <p>
+            <a href="#" class="text-dark">Ínicio</a>
+          </p>
+          <p>
+            <a href="#" class="text-dark">Quem somos?</a>
+          </p>
+          <p>
+            <a href="#" class="text-dark">Produtos</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold"><i class="fas fa-square-phone"></i> contato</h6>
+          <hr
+              class="mb-4 mt-0 d-inline-block mx-auto"/>
+          <p>
+            <a target="_blank" href="" class="text-dark">WhatsApp</a>
+          </p>
+          <p>
+            <a target="_blank" href="" class="text-dark">Instagram</a>
+          </p>
+          <p>
+            <a target="_blank" href="" class="text-dark">Email</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="text-center p-3 copy">
+    © 2023 Copyright:
+    <a class="" href="#">Mercadão barato</a>
+  </div>
+</footer>
--- src/pages/home/home.php
+++ src/pages/home/home.php
@@ -169,6 +169,8 @@
       <div class="main-division">
     </div>
   </main>
+
+  <? require_once "../../components/footer.php"; ?>
   
   <!-- MDB -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.0/mdb.min.js"></script>
